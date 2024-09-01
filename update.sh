daa70517c3b56179590307235d02a3f7085d2593
David Megginson <david.megginson@gmail.com>
2024-09-01 11:29:57 -0400

    Add initial FlightGear positioning.

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+DEST=$HOME/.local/share/scenery/local-scenery-mods/Objects/w090n40/w086n46/
+
+mkdir -p $DEST
+/bin/cp -v *.stg laker.ac laker.png $DEST
---
+++ view.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+fgfs --aircraft=ufo --lat=46.997139 --lon=-85.111278 --altitude=700 --heading=37
